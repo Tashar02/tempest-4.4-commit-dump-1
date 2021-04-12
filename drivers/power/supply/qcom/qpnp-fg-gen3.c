ce656c38275e21549e86a92fe4f74d034b1e0847
psndna88 <psndna88@gmail.com>
2021-04-12 09:24:25 +0530

    drivers: power: add timeouts to wakelocks
    These can get stuck sometimes and prevert system from sleeping.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -1344,7 +1344,7 @@ static int fg_awake_cb(struct votable *votable, void *data, int awake,
 	struct fg_chip *chip = data;
 
 	if (awake)
-		pm_stay_awake(chip->dev);
+		pm_wakeup_event(chip->dev, 500);
 	else
 		pm_relax(chip->dev);
 
@@ -2543,7 +2543,7 @@ static enum alarmtimer_restart fg_esr_filter_alarm_cb(struct alarm *alarm,
 	 * We cannot vote for awake votable here as that takes a mutex lock
 	 * and this is executed in an atomic context.
 	 */
-	pm_stay_awake(chip->dev);
+	pm_wakeup_event(chip->dev, 500);
 	schedule_work(&chip->esr_filter_work);
 
 	return ALARMTIMER_NORESTART;
@@ -4446,7 +4446,7 @@ static int fg_notifier_cb(struct notifier_block *nb,
 		 * We cannot vote for awake votable here as that takes
 		 * a mutex lock and this is executed in an atomic context.
 		 */
-		pm_stay_awake(chip->dev);
+		pm_wakeup_event(chip->dev, 500);
 		schedule_work(&chip->status_change_work);
 	}
 
@@ -5957,7 +5957,7 @@ static int fg_gen3_resume(struct device *dev)
 				msecs_to_jiffies(fg_sram_dump_period_ms));
 
 	if (!work_pending(&chip->status_change_work)) {
-		pm_stay_awake(chip->dev);
+		pm_wakeup_event(chip->dev, 500);
 		schedule_work(&chip->status_change_work);
 	}
 
