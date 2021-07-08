7d7d2822a57be5aee87317bf780bc1a5813df4a9
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-07-09 04:11:17 +0600

    Revert "input: fingerprint: Convert wakelock to wakeup source"
    This reverts commit fb5768d2a830db76bef84643315aac6a1adca800.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/fingerprint/fpc/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_platform_tee.c
@@ -35,6 +35,7 @@
 #include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
+#include <linux/wakelock.h>
 #include <linux/notifier.h>
 #include <linux/fb.h>
 #include <linux/mdss_io_util.h>
@@ -94,7 +95,7 @@ struct fpc1020_data {
 	struct pinctrl_state *pinctrl_state[ARRAY_SIZE(pctl_names)];
 	struct regulator *vreg[ARRAY_SIZE(vreg_conf)];
 
-	struct wakeup_source ttw_wl;
+	struct wake_lock ttw_wl;
 	int irq_gpio;
 	int rst_gpio;
 	struct mutex lock; /* To set/get exported values in sysfs */
@@ -635,7 +636,7 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 	dev_dbg(fpc1020->dev, "%s\n", __func__);
 
 	if (atomic_read(&fpc1020->wakeup_enabled)) {
-		__pm_wakeup_event(&fpc1020->ttw_wl, FPC_TTW_HOLD_TIME);
+		wake_lock_timeout(&fpc1020->ttw_wl, FPC_TTW_HOLD_TIME);
 	}
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
@@ -889,7 +890,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 
 	mutex_init(&fpc1020->lock);
 
-	wakeup_source_init(&fpc1020->ttw_wl, "fpc_ttw_wl");
+	wake_lock_init(&fpc1020->ttw_wl, WAKE_LOCK_SUSPEND, "fpc_ttw_wl");
 
 #ifdef CONFIG_TOUCHSCREEN_COMMON
 	fpc1020->input_handler.filter = input_filter;
@@ -933,7 +934,7 @@ static int fpc1020_remove(struct platform_device *pdev)
 	fb_unregister_client(&fpc1020->fb_notifier);
 	sysfs_remove_group(&pdev->dev.kobj, &attribute_group);
 	mutex_destroy(&fpc1020->lock);
-	wakeup_source_trash(&fpc1020->ttw_wl);
+	wake_lock_destroy(&fpc1020->ttw_wl);
 	(void)vreg_setup(fpc1020, "vdd_ana", false);
 #ifndef CONFIG_MACH_MI
 	(void)vreg_setup(fpc1020, "vdd_io", false);
