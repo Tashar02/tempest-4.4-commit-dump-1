da26c4aef4e1778d52ba917cf2af6ea3b997d3b4
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-07-10 01:59:21 +0600

    cpufreq: Set Schedhorizon as default CPU governor
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -510,17 +510,17 @@ void cpufreq_unregister_governor(struct cpufreq_governor *governor);
  * Performance governor is fallback governor if any other gov failed to auto
  * load due latency restrictions
  */
-#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
-extern struct cpufreq_governor cpufreq_gov_schedutil;
+#ifdef CONFIG_CPU_FREQ_GOV_SCHEDHORIZON
+extern struct cpufreq_governor cpufreq_gov_schedhorizon;
 #endif
-#ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+#ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDHORIZON
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedhorizon)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDHORIZON)
-extern struct cpufreq_governor cpufreq_gov_schedhorizon;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedhorizon)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
+extern struct cpufreq_governor cpufreq_gov_schedutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
 #endif
 
 
